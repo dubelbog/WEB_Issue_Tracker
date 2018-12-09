33470c31da59c1bf12328f9fdd48c3ffed88e7f5
Bogumila Dubel <dubelbog@students.zhaw.ch>
2018-12-09 12:02:24 +0100

    set PORT

--- web_server.js
+++ web_server.js
@@ -42,6 +42,6 @@ var server = http.createServer(function(request, response) {
     });
 });
 
-server.listen(8080, function(){
+server.listen(process.env.PORT || 5000, function(){
     console.log('ready captain!');
 });
